412d956342655081ae9c34649dd9cac4eb99249f
IdoShapira <ishapira@yotpo.com>
2018-12-17 14:01:04 +0200

    added create user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,58 @@
 class LoginController < ApplicationController
-  def login
+  require 'rest-client'
 
-    respond_to do |format|
-      format.html
-      format.js
+  def login
+    unless params[:request_type].nil?
+      respond_to do |format|
+        @message = login_handler
+        format.html
+        format.js
+      end
+    end
+  end
+
+  def login_handler
+    message = ''
+    case params[:request_type]
+    when 'login'
+      @user = User.find_by_email_playground("#{params[:playground]}&#{params[:email]}") # does the user exists
+      if !@user.nil?
+        if @user.verified == 1 # is the user verified
+          puts 'logged in'
+        else
+          puts 'not verified'
+        end
+      else
+        puts 'email does not exist with the attached playground'
+      end
+    when 'confirm_user'
+      2
+    when 'create_user'
+      # create the user locally
+      message=create_user
+      3
+    else
+      raise StandardError
+    end
+    message
+  end
+
+  def create_user
+    # create the user
+    begin
+      response=RestClient.post('localhost:8086/playground/users',
+                               {
+                                   "email": params[:email],
+                                   "playground": params[:playground],
+                                   "username": params[:user_name],
+                                   "avatar": params[:avatar],
+                                   "role": params[:role]
+                               }.to_json,{content_type: :json, accept: :json})
+      @user = User.create(email:params[:email],playground:params[:playground], username: params[:user_name],
+                          avatar: params[:avatar], verified: 0, points: 10000, role: params[:role])
+      return 'User Created'
+    rescue RestClient::ExceptionWithResponse => e
+      return "Error occured #{JSON.parse(e.response)['message']}"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_093215) do
+ActiveRecord::Schema.define(version: 2018_12_17_103835) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_17_093215) do
     t.integer "verified"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email_playground"
   end
 
 end
