262a58d75b82091ecbab2f6e1f652f8b0106607c
IdoShapira <ishapira@yotpo.com>
2018-12-22 18:21:24 +0200

    get all elements by attribute

--- app/controllers/api_admin_controller.rb
+++ app/controllers/api_admin_controller.rb
@@ -7,6 +7,7 @@ class ApiAdminController < ApplicationController
     #redirect_to '/api_user' if @user.role == 'Player' # activate after I create the same edit user in player section
     @elements = get_all_elements
     @element_edit = {}
+    @attr_elements = []
     unless params[:request_type].blank?
       case params[:request_type]
       when 'edit_user'
@@ -28,10 +29,47 @@ class ApiAdminController < ApplicationController
     when 'Get Element Data'
       get_element
     when 'Edit Element'
+      edit_element
     when 'Create Element'
       create_element
+    when 'Get All Element By Attribute'
+      puts 'im here'
+      get_elements_by_attribute
+    end
+  end
+
+  def get_elements_by_attribute
+    begin
+      response = RestClient.get("localhost:8086/playground/elements/#{@user.playground}/#{@user.email}/search/type/#{params[:element_attribute]}")
+      json_array = []
+      objArray = JSON.parse(response.body)
+      objArray.class
+      objArray.each do |json|
+        json_array << json
+      end
+      @attr_elements =json_array
+    rescue RestClient::ExceptionWithResponse => e
+      @message = "Error occured: Element not found"
+    rescue StandardError => e
+      @message =  "Error occured #{e}"
     end
 
+  end
+
+  def edit_element
+    attributes = set_attributes(params[:element_attributes_edit])
+    response = RestClient.put("localhost:8086/playground/elements/#{@user.playground}/#{@user.email}/#{@user.playground}/#{params[:element_id]}",{
+        "playground": "2019a.Sapir",
+        "id": params[:element_id],
+        "location": { "x": params[:element_location_x_edit], "y": params[:element_location_y_edit] },
+        "name": params[:name_element_edit],
+        "creationDate": params[:element_create_date_edit],
+        "expirationDate": params[:element_expiration_date_edit],
+        "attributes": attributes,
+        "creatorPlayground": @user.playground,
+        "creatorEmail": 'yaron@gmail.coms'
+    }.to_json,content_type: :json, accept: :json)
+    puts response.code
   end
 
   def get_element
@@ -40,8 +78,10 @@ class ApiAdminController < ApplicationController
       @element_edit = JSON.parse(response.body)
       @element_edit['attributes_print'] = ''
        @element_edit['attributes'].each do |h,v|
-         @element_edit['attributes_print'] << "#{h}:#{v}"
-      end
+         @element_edit['attributes_print'] << "#{h}:#{v},"
+       end
+      @element_edit['attributes_print'] = @element_edit['attributes_print'][0..-2] # remove last comma
+
     rescue RestClient::ExceptionWithResponse => e
       @message = "Error occured: Element not found"
     rescue StandardError => e
@@ -80,12 +120,12 @@ class ApiAdminController < ApplicationController
           "role": params[:role],
           "points": @user.points
       }.to_json,content_type: :json, accept: :json)
-      puts response.code
       if response.code == 200
         # update user on client
-        @user.update(params[:avatar], params[:role], params[:user_name], params[:points])
+        @user.update(params[:avatar], params[:role], params[:user_name], @user.points)
+        @message = 'User updated'
       end
-      puts 'isdf'
+
     rescue RestClient::ExceptionWithResponse => e
       @message = "Error occured #{JSON.parse(e.response)['message']}"
     rescue StandardError => e
@@ -95,13 +135,8 @@ class ApiAdminController < ApplicationController
 
   def create_element
     @user = User.find_by_email(session[:user])
-    attributes = {}
     location = {}
-    attributes_data = params[:attributes].split(',')
-    attributes_data.each do |data|
-      attr=data.split(':')
-      attributes[attr[0]] = attr[1]
-    end
+    attributes = set_attributes(params[:attributes])
     location['x'] = params[:location_x] unless params[:location_x].blank?
     location['y'] = params[:location_y] unless params[:location_y].blank?
 
@@ -125,6 +160,16 @@ class ApiAdminController < ApplicationController
     rescue StandardError => e
       @message = "Error occured #{e.message}"
     end
+  end
+
+  def set_attributes(form_attributes)
+    attributes = {}
+    attributes_data = form_attributes.split(',')
+    attributes_data.each do |data|
+      attr=data.split(':')
+      attributes[attr[0]] = attr[1]
+    end
+    attributes
   end
 
 
